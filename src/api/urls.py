754b11829ab94915ee5d4006d0581795dd5ca4f1
Nikola Maric <nmaric82@gmail.com>
2023-07-14 16:23:00 +0200

    make it pretty looking

--- src/api/urls.py
+++ src/api/urls.py
@@ -5,14 +5,6 @@ from api.views import TableView, DynamicTableRowCreateView, DynamicTableRowListV
 urlpatterns = [
     path("table", TableView.as_view(), name="create_dynamic_table"),
     path("table/<int:table_id>", TableView.as_view(), name="update_dynamic_table"),
-    path(
-        "table/<int:table_id>/row",
-        DynamicTableRowCreateView.as_view(),
-        name="create_dynamic_table_row",
-    ),
-    path(
-        "table/<int:table_id>/rows",
-        DynamicTableRowListView.as_view(),
-        name="list_dynamic_table_rows",
-    ),
+    path("table/<int:table_id>/row", DynamicTableRowCreateView.as_view(), name="create_dynamic_table_row"),
+    path("table/<int:table_id>/rows", DynamicTableRowListView.as_view(), name="list_dynamic_table_rows"),
 ]
